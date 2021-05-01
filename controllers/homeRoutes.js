692e438025fe2546cee2e63b630982c4e315dc93
Dylancouzon <dylancouzon@gmail.com>
2021-04-30 20:48:18 -0700

    Add homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
