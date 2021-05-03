b136be07a67af9b59cbb18e05cac8fc0c39c49a9
Dylancouzon <dylancouzon@gmail.com>
2021-05-03 15:38:02 -0700

    Maybe ?

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ app.use(session(sess));
 
 
 app.engine('handlebars', hbs.engine);
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(routes);
 
